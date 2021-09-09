d2f6ea30dcfb7f1c73399d3cf48fe79c21f1ab3d
mailllonely <maillonely@gmail.com>
2021-09-09 13:09:39 -0500

    Add files via upload

---
+++ external_packages.R
@@ -0,0 +1,11 @@
+check_Rcpp = require("Rcpp")
+
+if (check_Rcpp == FALSE){ install.packages("Rcpp")}
+check_RcppArmadillo = require("RcppArmadillo")
+if (check_RcppArmadillo == FALSE){ install.packages("RcppArmadillo")}
+check_combinat = require("combinat")
+if (check_combinat == FALSE){ install.packages("combinat")}
+check_RColorBrewer = require("RColorBrewer")
+if (check_RColorBrewer == FALSE){ install.packages("RColorBrewer")}
+check_gplots = require("gplots")
+if (check_gplots == FALSE){ install.packages("gplots")}
